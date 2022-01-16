3f2d14d223c69b5270a6b02c5fda633963e6a4c2
Vitor Fontenele <vitor_fontenele@hotmail.com>
2022-01-16 00:10:29 -0300

    Adição do código e malha do Problema de Poiseuille

---
+++ validacao/malhaModulo.py
@@ -0,0 +1,145 @@
+import meshio
+import numpy as np
+import math
+
+class Malha:
+    def __init__(self,fileName):
+        self.fileName = fileName
+        self.elSizeFactor = 0.05
+        self.msh = meshio.read(self.fileName)
+        self.X = self.msh.points[:,0]
+        self.Y = self.msh.points[:,1]
+        self.Lx = max(self.X) - min(self.X)
+        self.Ly = max(self.Y) - min(self.Y)
+        self.nx = int(math.ceil(self.Lx/self.elSizeFactor)+1)
+        self.ny = int(math.ceil(self.Ly/self.elSizeFactor)+1)
+        self.IEN = self.construirIEN()
+        self.IENBound = self.construirIENBound()
+        self.cc = np.unique(self.IENBound.reshape(self.IENBound.size))
+        self.ne = len(self.IEN)
+        self.npoints = len(self.X)
+
+    def construirIEN(self):
+        for cell in self.msh.cells:
+            if cell.type == "triangle":
+                IEN = cell.data
+        return IEN
+
+    def construirIENBound(self):
+        for cell in self.msh.cells:
+            if cell.type == "line":
+                IENBound = cell.data
+        return IENBound
+
+class MatrizesGlobais(Malha):
+    def __init__(self,fileName):
+        super().__init__(fileName)
+        self.K = np.zeros( (self.npoints,self.npoints), dtype='double')
+        self.M = np.zeros( (self.npoints,self.npoints), dtype='double')
+        self.GX = np.zeros( (self.npoints,self.npoints), dtype='double')
+        self.GY = np.zeros( (self.npoints,self.npoints), dtype='double')
+        self.construirMatrizes()
+        self.Kest = np.zeros( (self.npoints,self.npoints), dtype='double')
+
+    def construirMatrizes(self):
+        for e in range(0,self.ne):
+
+            """
+            Matrizes do elemento
+            """
+
+            v1 = self.IEN[e,0]
+            v2 = self.IEN[e,1]
+            v3 = self.IEN[e,2]
+
+            area = (1.0/2.0)*abs(np.linalg.det( np.array([[1.0,self.X[v1],self.Y[v1]],
+                                            [1.0,self.X[v2],self.Y[v2]],
+                                            [1.0,self.X[v3],self.Y[v3]]]) ))
+
+            bi = self.Y[v2] - self.Y[v3]
+            bj = self.Y[v3] - self.Y[v1]
+            bk = self.Y[v1] - self.Y[v2]
+            ci = self.X[v3] - self.X[v2]
+            cj = self.X[v1] - self.X[v3]
+            ck = self.X[v2] - self.X[v1]
+                
+            B = (1.0/(2.0*area))*np.array([[bi,bj,bk],
+                                            [ci,cj,ck]])
+            BT = np.transpose(B)
+            kelem = area * np.dot(BT,B)
+
+            melem = (area/12.0)*np.array([[2.0, 1.0, 1.0],
+                                [1.0, 2.0, 1.0],
+                                [1.0, 1.0, 2.0]])
+
+            gxelem = 1/6*np.array([[bi, bj, bk],
+                        [bi, bj, bk],
+                        [bi, bj, bk]])
+    
+            gyelem = 1/6*np.array([[ci, cj, ck],
+                        [ci, cj, ck],
+                        [ci, cj, ck]])
+            
+            """
+            Matrizes K, M, GX e GY globais
+            """
+
+            for ilocal in range(0,3):
+                iglobal = self.IEN[e,ilocal]
+                for jlocal in range(0,3):
+                    jglobal = self.IEN[e,jlocal]
+
+                    self.K[iglobal,jglobal] = self.K[iglobal,jglobal] + kelem[ilocal,jlocal]
+                    self.M[iglobal,jglobal] = self.M[iglobal,jglobal] + melem[ilocal,jlocal]
+                    self.GX[iglobal,jglobal] = self.GX[iglobal,jglobal] + gxelem[ilocal,jlocal]
+                    self.GY[iglobal,jglobal] = self.GY[iglobal,jglobal] + gyelem[ilocal,jlocal]
+                    
+    def construirMatrizKest(self,vx,vy,dt):
+        for e in range(0,self.ne):
+            
+            """
+            Matrizes do elemento
+            """
+
+            v1 = self.IEN[e,0]
+            v2 = self.IEN[e,1]
+            v3 = self.IEN[e,2]
+          
+            vxbarra = (vx[int(v1)]+vx[int(v2)]+vx[int(v3)])/3
+            vybarra = (vy[int(v1)]+vy[int(v2)]+vy[int(v3)])/3
+            
+            area = (1.0/2.0)*abs(np.linalg.det( np.array([[1.0,self.X[v1],self.Y[v1]],
+                                            [1.0,self.X[v2],self.Y[v2]],
+                                            [1.0,self.X[v3],self.Y[v3]]]) ))
+
+            bi = self.Y[v2] - self.Y[v3]
+            bj = self.Y[v3] - self.Y[v1]
+            bk = self.Y[v1] - self.Y[v2]
+            ci = self.X[v3] - self.X[v2]
+            cj = self.X[v1] - self.X[v3]
+            ck = self.X[v2] - self.X[v1]
+            
+            kx = (1/(4*area))*np.array([[bi**2,bi*bj,bi*bk],
+                                        [bj*bi,bj**2,bj*bk],
+                                    [bk*bi,bk*bj,bk**2]])
+            
+            ky = (1/(4*area))*np.array([[ci**2,ci*cj,ci*ck],
+                                        [cj*ci,cj**2,cj*ck],
+                                        [ck*ci,ck*cj,ck**2]])
+            
+            kxy = (1/(4*area))*np.array([[bi*ci,bi*cj,bi*ck],
+                                        [bj*ci,bj*cj,bj*ck],
+                                        [bk*ci,bk*cj,bk*ck]])
+            
+            kest = vxbarra*dt/2*(vxbarra*kx+vybarra*kxy) + vybarra*dt/2*(vxbarra*kxy+vybarra*ky)
+
+            """
+            Matriz Kest Global
+            """
+            
+            for ilocal in range(0,3):
+                iglobal = self.IEN[e,ilocal]
+                for jlocal in range(0,3):
+                    jglobal = self.IEN[e,jlocal]
+                    self.Kest[iglobal,jglobal] = self.Kest[iglobal,jglobal] + kest[ilocal,jlocal]
+
---
+++ validacao/poiseuille.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 03:38:05 2021
+
+@author: Vitor Fontenele
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.tri as mtri
+from shapely.geometry import Point, Polygon
+
+"""
+Caracteristicas basicas da malha
+"""
+
+fileName = "malhapoiseuille.msh"
+
+import malhaModulo
+
+malha = malhaModulo.Malha(fileName)
+
+X = malha.X
+Y = malha.Y
+Lx = malha.Lx
+Ly = malha.Ly
+nx = malha.nx
+ny = malha.ny
+IEN = malha.IEN
+IENBound = malha.IENBound
+cc = malha.cc
+ne = malha.ne
+npoints = malha.npoints
+
+"""
+Pontos de contorno e criacao da bval
+"""
+
+c1 = 0.0
+c2 = 1.0
+dc = c2 - c1
+
+bval = np.zeros(npoints,dtype = 'float')
+vx = np.zeros(npoints,dtype = 'float')
+vy = np.zeros(npoints,dtype = 'float')
+
+#cantos da malha
+bval[0] = c1
+bval[1] = c2
+bval[2] = c2
+bval[3] = c1
+
+start = 4
+ignore = []
+
+for k1 in range (ny-2):
+    #contorno direito
+    ignore.append(k1 + start)
+k1 += 1
+
+for k2 in range (nx-2):
+    #contorno superior
+    bval[k2 + k1 + start] = c2
+k2 += 1
+
+for k3 in range (ny-2):
+    #contorno esquerdo
+    bval[k3 + k2 + k1 + start] = dc/Ly*Y[k3 + k2 + k1 + start]+c1-Y[0]*dc/Ly
+k3 += 1
+    
+for k4 in range (nx-2):
+    #contorno inferior
+    bval[k4 + k3 + k2 + k1 + start] = c1
+
+"""
+Matrizes Globais (K, M, GX e GY)
+"""
+
+matrizesGlobais = malhaModulo.MatrizesGlobais(fileName)
+K = matrizesGlobais.K
+M = matrizesGlobais.M
+GX = matrizesGlobais.GX
+GY = matrizesGlobais.GY
+
+"""
+Parametros utilizados
+"""
+
+dt = 0.01
+Re = 100
+iteracoes = 1
+
+"""
+Solucao utilizando eq de transporte e de funcao corrente
+"""
+
+#matriz identidade
+ident = np.identity(npoints)
+
+for g in range (iteracoes):
+    #Condicao de Contorno de Vorticidade
+    B1 = np.dot(GX,vy) - np.dot(GY,vx)
+    wz = np.linalg.solve(M,B1)
+
+    matrizesGlobais.construirMatrizKest(vx,vy,dt)
+    Kest = matrizesGlobais.Kest
+
+    #Solucao da equacao de transporte
+    vx_id = ident*vx
+    vy_id = ident*vy
+    A2 = M/dt 
+    B2 = np.dot((M/dt - np.dot(vx_id,GX) - np.dot(vy_id,GY) - K/Re),wz) - np.dot(Kest,wz)
+    for i in cc:
+        A2[i,:] = 0.0
+        B2[i] = wz[i]
+        A2[i,i] = 1.0  
+    wz = np.linalg.solve(A2,B2)
+    
+    #Solucao da equacao de funcao corrente
+    A3 = K
+    B3 = np.dot(M,wz) 
+    for i in cc:
+        #ignore = [4,5,6,7,8,9,10,11,12]
+        if i not in ignore:
+            A3[i,:] = 0.0
+            B3[i] = bval[i]
+            A3[i,i] = 1.0   
+                
+    Psi = np.linalg.solve(A3,B3)
+    
+    #Atualizacao de vx e vy
+    #vx
+    A4 = M
+    B4 = np.dot(GY,Psi)
+    vx = np.linalg.solve(A4,B4)
+    #vy
+    A5 = M
+    B5 = np.dot(-GX,Psi)
+    vy = np.linalg.solve(A5,B5)
+    
+    #Imposicao das cc de vx e vy      
+    vx[0] = 0
+    vx[1] = 0
+    vx[2] = 0
+    vx[3] = 0
+    
+    vy[0] = 0
+    vy[1] = 0
+    vy[2] = 0
+    vy[3] = 0
+
+    start = 4
+    
+    for k1 in range (ny-2):
+        #contorno direito
+        vy[k1 + start] = 0
+        #vx = ...    
+    k1 += 1
+
+    for k2 in range (nx-2):
+        #contorno superior
+        vx[k2 + k1 + start] = 0
+        vy[k2 + k1 + start] = 0
+    k2 += 1
+
+    for k3 in range (ny-2):
+        #contorno esquerdo
+        vx[k3 + k2 + k1 + start] = 1 #atencao
+        vy[k3 + k2 + k1 + start] = 0
+    k3 += 1
+    
+    for k4 in range (nx-2):
+        #contorno inferior
+        vx[k4 + k3 + k2 + k1 + start] = 0
+        vy[k4 + k3 + k2 + k1 + start] = 0   
+
+#Setup do plot
+plt.rc('text', usetex=True)
+triang = mtri.Triangulation(X,Y,IEN)
+ax = plt.axes()
+ax.set_aspect("equal")
+
+"""O que voce quer plotar?"""
+#--Funcao corrente --> Psi
+#--Vorticidade --> wz
+#--Velocidade em x --> vx
+#--Velocidade em y --> vy
+ax.tricontourf(triang,Psi,levels=100,cmap = 'jet')
+plt.title("Função corrente")
+
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+
+plt.show()
+#plt.savefig("poiseuille.png",dpi=300)
